9ba303058d58388fb8eedad162f7963466701ac7
tonymause <tonymause@163.com>
2016-01-04 10:40:33 +0800

    added bootstrap jquery

--- cleancode.ui/Startup.cs
+++ cleancode.ui/Startup.cs
@@ -21,7 +21,6 @@ namespace cleancode.ui
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
             app.UseStaticFiles();
             app.UseMvc(config =>
             {
