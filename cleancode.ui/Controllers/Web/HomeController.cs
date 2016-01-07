ee7eb3831bdc7d10f7859ce301197180b9179f5a
tonymause <tonymause@163.com>
2016-01-07 14:47:08 +0800

    use mvc, added controller  and view

---
+++ cleancode.ui/Controllers/Web/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace cleancode.ui.Controllers.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- cleancode.ui/Startup.cs
+++ cleancode.ui/Startup.cs
@@ -15,16 +15,20 @@ namespace cleancode.ui
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseMvc(config =>
             {
-                await context.Response.WriteAsync("Hello Azure!");
+                config.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", Action = "Index" });
             });
         }
 
